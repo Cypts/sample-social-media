46db7b5eb9154b03ecd6572eeb14e310439ddbc7
ayush_sinha <you@example.com>
2025-06-23 17:07:44 +0530

    added pagingination

--- home.php
+++ home.php
@@ -1,4 +1,30 @@
 <link href="css/style.css" rel="stylesheet">
+<style>
+   .pagination{
+    display: flex;
+    justify-content: center;
+    height: 40px;
+    list-style-type: none;
+    
+} 
+    .pagination li{
+        min-width: 40px;
+        background-color: brown;
+        
+        margin: 0 5px;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+    .pagination li a{
+        text-decoration: none;
+        color:white;
+        font-size: larger;
+        font-weight: bolder;
+    }
+</style>
 <?php
 session_start();
 if (!isset($_SESSION['userid'])) {
@@ -29,14 +55,34 @@ function timeAgo($times) {
 
 $uid = $_SESSION['userid'];
 $now = new DateTime();
+if(isset($_GET['page'])){
+    $page=$_GET['page'];
+}
+else{
+    $page=1;
+}
+$limit=5;
+
+$sql2 = "SELECT p.content, p.post_time, u.name, u.username,u.id
+        FROM post p
+        JOIN users u ON p.user_id = u.id
+        JOIN friends f ON f.user_id = '$uid' AND f.friend_id = u.id 
+        WHERE f.friend_id IS NOT NULL
+        ORDER BY p.post_time DESC";
+
+$rs2 = mysqli_query($conn, $sql2);
+$num= mysqli_num_rows($rs2);
+$total_pages= ceil($num/$limit);
+$offset=($page-1)*$limit;
+
 
 
 $sql = "SELECT p.content, p.post_time, u.name, u.username,u.id
         FROM post p
         JOIN users u ON p.user_id = u.id
-        LEFT JOIN friends f ON f.user_id = '$uid' AND f.friend_id = u.id
+        JOIN friends f ON f.user_id = '$uid' AND f.friend_id = u.id 
         WHERE f.friend_id IS NOT NULL
-        ORDER BY p.post_time DESC";
+        ORDER BY p.post_time DESC limit $offset, $limit";
 
 $rs = mysqli_query($conn, $sql);
 
@@ -57,3 +103,11 @@ while ($row = mysqli_fetch_array($rs)) {
     echo '</div>';
 }
 ?>
+<ul class="pagination">
+    <?php
+    for($i = 1 ; $i <= $total_pages ; $i++)
+    {
+        echo '<li><a href=home.php?page='.$i.'>'.$i.'<a></li>';
+    }
+    ?>
+</ul>
